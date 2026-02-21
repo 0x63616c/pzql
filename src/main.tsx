2da022e6a776bcf792bdad1eb5dff9e90f835819
Calum <6991398+0x63616c@users.noreply.github.com>
2026-02-21 12:58:11 -0800

    fix: rename hyper theme import to hyperNight

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { applyTheme } from "./themes";
-import { hyper } from "./themes/hyper";
+import { hyperNight } from "./themes/hyper";
 
-applyTheme(hyper);
+applyTheme(hyperNight);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
